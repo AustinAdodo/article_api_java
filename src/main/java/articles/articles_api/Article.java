fb96447867174c4eb413a855f839e93b9f7eac8e
Odia Adodo <subzbelow@gmail.com>
2023-09-27 15:33:29 +0100

    Added Article service to be autowired

--- src/main/java/articles/articles_api/Article.java
+++ src/main/java/articles/articles_api/Article.java
@@ -1,13 +1,15 @@
 package articles.articles_api;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Article {
+    @Getter
     private String title;
+    private String Body;
+    private int id;
 
     public Article(String title) {
         this.title = title;
-    }
-
-    public String getTitle() {
-        return this.title;
     }
 }
--- src/main/java/articles/articles_api/ArticleService.java
+++ src/main/java/articles/articles_api/ArticleService.java
@@ -1,4 +1,7 @@
 package articles.articles_api;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ArticleService {
 }
