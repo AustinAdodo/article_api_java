d5f48d04d67812c1ee6eed7212dfcc3bb2107ebb
Odia Adodo <subzbelow@gmail.com>
2023-10-24 12:04:30 +0100

    Updted Test file

--- src/test/java/articles/articles_api/ApplicationTest.java
+++ src/test/java/articles/articles_api/ApplicationTest.java
@@ -1,39 +1,36 @@
 package articles.articles_api;
 
-//package articles;
-
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.BeforeClass;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ApplicationTest {
-    private static List<Article> articles = new ArrayList<Article>();
-    private static ArticleService service = new ArticleService();
+    private static final List<Article> articles = new ArrayList<Article>();
+    private static final ArticleService service = new ArticleService();
 
     @Autowired
+    /**
+     * Injects the mock Library.
+     */
     private MockMvc mockMvc;
 
     @BeforeClass
@@ -45,15 +42,22 @@ public class ApplicationTest {
 
     @Before
     public void clearDB() {
-        this.service.clear();
+        service.clear();
     }
 
     public void addArticles() {
         for (Article article : articles) {
-            this.service.add(article);
+            service.add(article);
         }
     }
 
+    /**
+     * Test showing articles can be removed.
+     *
+     * @param {this.mockMvc} injected dependency from mock library.
+     * @param {article}      article instance.
+     * @return 200 OK, JSON_UTF8.
+     */
     @Test
     public void shouldLetUsPostArticles() throws Exception {
         for (Article article : articles) {
@@ -61,7 +65,7 @@ public class ApplicationTest {
                             .content(asJsonString(article))
                             .contentType(MediaType.APPLICATION_JSON)
                             .accept(MediaType.APPLICATION_JSON))
-                    .andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)) //TestUtil.APPLICATION_JSON_UTF8
                     .andExpect(jsonPath("title", is(article.getTitle())));
         }
     }
@@ -70,7 +74,7 @@ public class ApplicationTest {
     public void shouldAllowUpdatingArticles() throws Exception {
         addArticles();
         String body = "This is some filler text for a killer article";
-        Article article = this.service.getAll().get(0);
+        Article article = service.getAll().get(0);
         Integer id = (Integer) article.getId();
         if (id == null) {
             article.setId(0);
@@ -82,36 +86,37 @@ public class ApplicationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent());
-        Article actual = this.service.findById(article.getId());
+        Article actual = service.findById(article.getId()); //getId returns int.
         assertEquals("Should have updated the article", actual.getBody(), body);
     }
 
     @Test
     public void shouldAllowUsToRemoveArticles() throws Exception {
         addArticles();
-        List<Article> all = new ArrayList<Article>(this.service.getAll());
+        List<Article> all = new ArrayList<Article>(service.getAll());
         for (Article article : all) {
             Integer id = (Integer) article.getId();
             if (id == null) {
                 article.setId(0);
             }
-
             this.mockMvc.perform(delete("/articles/" + article.getId()))
                     .andExpect(status().isNoContent());
         }
-        assertEquals("Should remove all articles", 0, this.service.getAll().size());
+        assertEquals("Should remove all articles", 0, service.getAll().size());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
 
-//    public static String asJsonString(final Object obj) {
-//        try {
-//            final ObjectMapper mapper = new ObjectMapper();
-//            final String jsonContent = mapper.writeValueAsString(obj);
-//            return jsonContent;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+
+
 
