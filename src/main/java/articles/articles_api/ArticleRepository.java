db0901d248b96fd263a1f1b0cf57baa5fdbe32f9
Odia Adodo <subzbelow@gmail.com>
2023-10-24 12:39:31 +0100

    specificed table to work with ORM

--- src/main/java/articles/articles_api/ArticleRepository.java
+++ src/main/java/articles/articles_api/ArticleRepository.java
@@ -1,9 +1,13 @@
 package articles.articles_api;
 
+import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//add spring-boot-starter-data-jpa to Pom.xml file
+
 @Repository
+@Table(name = "articles")
 public abstract class ArticleRepository implements JpaRepository<Article, Long> {
 }
+
+//add spring-boot-starter-data-jpa to Pom.xml file
