47f5ee2c684fcead4e7f2cbb1959eda58f573ce5
Odia Adodo <subzbelow@gmail.com>
2023-10-26 11:19:07 +0100

    Added @Transactional

--- src/main/java/articles/articles_api/ArticleService.java
+++ src/main/java/articles/articles_api/ArticleService.java
@@ -1,5 +1,6 @@
 package articles.articles_api;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,24 +16,25 @@ public class ArticleService {
     public ArticleService(ArticleRepository repo) {
         this.repo = repo;
     }
-    public ArticleService() {
 
-    }
+    public ArticleService() {}
 
+    @Transactional
     public List<Article> getAll() {
-        List<Article> all = repo.findAll();
-        if (!all.isEmpty()) {
-            return all;
-        }
+        List<Article> all = new ArrayList<>();
+        if (this.repo != null) { all = repo.findAll();}
+        if (!all.isEmpty()) {return all;}
         List<Article> emptyList = new ArrayList<>();
         return emptyList;
     }
 
+    @Transactional
     public Article findById(int id) {
         Integer Id = id;
-        return repo.findById((long) id).get();
+        return repo.findById(id).get();
     }
 
+    @Transactional
     public void remove(int id) {
         try {
             Article article = findById(id);
@@ -42,20 +44,28 @@ public class ArticleService {
         }
     }
 
+    @Transactional
     public Article add(Article article) {
 //        Article current_article =  findById(article.id);
-        if (article != null) {
+        if (article != null && this.repo!=null) {
+            repo.save(article);
+            return article;
+        }
+        return new Article();
+    }
+
+    @Transactional
+    public void UpdateArticle(Article article) {
+        if (this.repo != null) {
             repo.save(article);
         }
-        return article;
     }
 
-    public void UpdateArticle(Article article) {
-        repo.save(article);
-    }
-
+    @Transactional
     public void clear() {
-        this.repo.flush();
+        if (this.repo != null) {
+            this.repo.flush();
+        }
     }
 
     public void deleteAll() {
